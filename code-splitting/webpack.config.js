9eeefb3c0135b45ce2d013ef8f91d07622838581
BrianH <brianhwang29@gmail.com>
2021-08-05 14:12:45 +0900

    : commit

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'none', //production, development, none
@@ -11,7 +12,10 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader"
+        ]
       },
       /*{
         test : /\/js$/,
@@ -23,4 +27,15 @@ module.exports = {
       // }
     ]
   },
+  /**
+   * 플러그인은 웹팩의 기본적인 동작에 추가적인 기능을 제공하는 속성이다.
+   * 로더랑 비교하면 로더는 파일을 해석하고 변환하는 과정에 관여하는 반면, 
+   * 플러그인은 해당 결과물의 형태를 바꾸는 역할을 한다.
+   * 
+   * 플러그인 전에는 main.css 가 bundle.gs에 포함 되어져 있었는데
+   * 플러그인 이후 main.css 파일이 따로 생성 된다.
+   */
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
 }
